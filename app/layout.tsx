3309fe179197ae7ce405a37a8134d50386ca9b97
M <48407846+mvahaste@users.noreply.github.com>
2023-05-16 10:54:18 +0300

    *

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,10 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigge
 import { Laptop, Moon, Sun } from "lucide-react";
 import { Inter } from "next/font/google";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import "./globals.css";
+
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 // const roboto = Roboto({
@@ -21,59 +24,12 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	const bodyClasses = "min-h-screen antialiased vsc-initialized";
+
 	return (
 		<html lang="en">
 			<title>{metadata.title}</title>
 			<body className={(inter.className, bodyClasses)}>
-				{/* Header */}
-				<header className="mx-auto w-full flex flex-row items-center border-b-[1px] py-2 h-14 sticky top-0 z-40 bg-white bg-opacity-75 backdrop-blur-md shadow-sm">
-					<div className="flex container max-w-[85rem] px-4 mx-auto items-center">
-						<div className="flex gap-2">
-							<Avatar className="w-6 h-6">
-								<AvatarImage src="https://github.com/mvahaste.png?size=40" alt="@mvahaste" />
-								<AvatarFallback>MV</AvatarFallback>
-							</Avatar>
-							<h1 className="font-bold">mvahaste.dev</h1>
-						</div>
-						<div className="grow flex justify-between">
-							<div className="flex items-center justify-center gap-8 ml-8 text-sm font-medium">
-								<Link href="/" className="transition-colors hover:text-foreground/80 text-foreground/60">
-									Home
-								</Link>
-								<Link href="/projects" className="transition-colors hover:text-foreground/80 text-foreground/60">
-									Projects
-								</Link>
-								<Link href="/about" className="transition-colors hover:text-foreground/80 text-foreground/60">
-									About
-								</Link>
-								<Link href="/contact" className="transition-colors hover:text-foreground/80 text-foreground/60">
-									Contact
-								</Link>
-							</div>
-							<div>
-								<DropdownMenu>
-									<DropdownMenuTrigger asChild>
-										<Button variant="ghost">Theme</Button>
-									</DropdownMenuTrigger>
-									<DropdownMenuContent>
-										<DropdownMenuItem onClick={() => setTheme("light")}>
-											<Sun className="mr-2 h-4 w-4" />
-											Light
-										</DropdownMenuItem>
-										<DropdownMenuItem onClick={() => setTheme("dark")}>
-											<Moon className="mr-2 h-4 w-4" /> Dark
-										</DropdownMenuItem>
-										<DropdownMenuItem onClick={() => setTheme("system")}>
-											<Laptop className="mr-2 h-4 w-4" />
-											System
-										</DropdownMenuItem>
-									</DropdownMenuContent>
-								</DropdownMenu>
-							</div>
-						</div>
-					</div>
-				</header>
-				{/* Content */}
+				<Header />
 				<div className="lg:pt-6 lg:pb-10 lg:max-w-[86rem] px-6 sm:max-w-full mx-auto">{children}</div>
 			</body>
 		</html>
--- components/header.tsx
+++ components/header.tsx
@@ -2,44 +2,65 @@
 
 import { Laptop, Moon, Sun } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
 
-export default function header() {
+const Header = () => {
 	return (
-		<div className="mx-auto w-full flex flex-row items-center border-b-[1px] lg:px-72 py-2 h-14 fixed bg-white bg-opacity-75 backdrop-blur-md">
-			<h1 className="font-bold">mvahaste.dev</h1>
-			<div className="grow flex justify-between">
-				<div className="flex items-center justify-center gap-8 ml-8 text-sm">
-					<Link href="/projects">Projects</Link>
-					<Link href="/about">About</Link>
-					<Link href="/contact">Contact</Link>
+		<header className="mx-auto w-full flex flex-row items-center border-b-[1px] py-2 h-14 sticky top-0 z-40 bg-white bg-opacity-75 backdrop-blur-md shadow-sm">
+			<div className="flex container max-w-[85rem] px-4 mx-auto items-center">
+				<div className="flex gap-2 items-center">
+					<Avatar className="w-6 h-6">
+						<AvatarImage src="https://github.com/mvahaste.png?size=40" alt="@mvahaste" />
+						<AvatarFallback>MV</AvatarFallback>
+					</Avatar>
+					<span className="hidden font-bold sm:inline-block">mvahaste.dev</span>
 				</div>
-				<div>
-					<DropdownMenu>
-						<DropdownMenuTrigger asChild>
-							<Button variant="ghost">Open</Button>
-						</DropdownMenuTrigger>
-						<DropdownMenuContent>
-							<DropdownMenuItem onClick={() => setTheme("light")}>
-								<Sun className="mr-2 h-4 w-4" />
-								Light
-							</DropdownMenuItem>
-							<DropdownMenuItem onClick={() => setTheme("dark")}>
-								<Moon className="mr-2 h-4 w-4" /> Dark
-							</DropdownMenuItem>
-							<DropdownMenuItem onClick={() => setTheme("system")}>
-								<Laptop className="mr-2 h-4 w-4" />
-								System
-							</DropdownMenuItem>
-						</DropdownMenuContent>
-					</DropdownMenu>
+				<div className="grow flex justify-between">
+					<nav className="flex items-center justify-center gap-8 ml-8 text-sm font-medium">
+						<Link href="/" className="transition-colors hover:text-foreground/80 text-foreground/60">
+							Home
+						</Link>
+						<Link href="/projects" className="transition-colors hover:text-foreground/80 text-foreground/60">
+							Projects
+						</Link>
+						<Link href="/about" className="transition-colors hover:text-foreground/80 text-foreground/60">
+							About
+						</Link>
+						<Link href="/contact" className="transition-colors hover:text-foreground/80 text-foreground/60">
+							Contact
+						</Link>
+					</nav>
+					<div>
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<Button variant="ghost">Theme</Button>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent>
+								<DropdownMenuItem onClick={() => setTheme("light")}>
+									<Sun className="mr-2 h-4 w-4" />
+									Light
+								</DropdownMenuItem>
+								<DropdownMenuItem onClick={() => setTheme("dark")}>
+									<Moon className="mr-2 h-4 w-4" /> Dark
+								</DropdownMenuItem>
+								<DropdownMenuItem onClick={() => setTheme("system")}>
+									<Laptop className="mr-2 h-4 w-4" />
+									System
+								</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</div>
 				</div>
 			</div>
-		</div>
+		</header>
 	);
-}
+};
 
 function setTheme(theme: string): void {
 	console.log(theme);
 }
+
+export default Header;
