327bb6911398a19a9949b911ed79114b05efc41b
M <48407846+mvahaste@users.noreply.github.com>
2023-05-18 13:12:40 +0300

    Added dynamic* public/private icons
    Now the GitHub repo is being fetched on load and if the status code is 200 then it is marked as public.

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,33 +1,32 @@
 import { Badge } from "@/components/ui/badge";
 import { Project } from "@/lib/projects";
 import { ExternalLink, Lock, Unlock } from "lucide-react";
-
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
-
 import Image from "next/image";
 import CustomTooltip from "./custom-tooltip";
 
-const ProjectCard = ({ project }: { project: Project }) => {
+const ProjectCard = async ({ project }: { project: Project }) => {
+	var projectPublic = await ProjectPublic(project.link);
+
 	return (
-		<div className="bg-card border drop-shadow rounded-lg w-full md:w-80 flex flex-col pb-7 z-30">
+		<div className="bg-card border drop-shadow rounded-lg flex flex-col pb-7 z-30">
 			<a href={project.image} target="_blank">
 				<Image
 					quality={100}
 					src={project.image}
-					width={300}
-					height={300}
+					width={512}
+					height={512}
 					alt={project.title + "thumbnail"}
-					className="border-b rounded-tl-lg rounded-tr-lg aspect-square object-top object-cover w-full h-auto shadow-inner"
+					className="border-b rounded-tl-lg rounded-tr-lg aspect-[6/5] object-top object-cover w-full h-auto shadow-inner"
 				/>
 			</a>
 			<div className="flex flex-col px-3 pb-3 pt-2">
-				<a href={project.link} target="_blank" className="scroll-m-20 text-xl font-semibold group tracking-tight hover:underline text-card-foreground">
+				<a href={project.link} target="_blank" className="scroll-m-20 text-lg font-semibold group tracking-tight hover:underline text-card-foreground">
 					{project.title}
 					<span className="invisible group-hover:visible">
 						<ExternalLink className="inline-block ml-1 h-5 w-5 mb-1" />
 					</span>
 				</a>
-				<p className="leading-7 [&:not(:first-child)]:mt-0.5 mb-1 text-card-foreground">{project.description}</p>
+				<p className="leading-7 [&:not(:first-child)]:mt-0.5 mb-1 text-card-foreground text-sm">{project.description}</p>
 				<div className="flex justify-end gap-3 absolute bottom-3 right-3">
 					{project.tags.map((tag) => (
 						<Badge key={tag} variant="outline">
@@ -35,13 +34,17 @@ const ProjectCard = ({ project }: { project: Project }) => {
 						</Badge>
 					))}
 				</div>
-				<div className="w-6 h-6 absolute bottom-2.5 left-2.5 flex items-center justify-center group" title={`This project is ${project.public ? "public" : "private"}`}>
-					{project.public ? <Unlock size={20} /> : <Lock size={20} />}
-					<CustomTooltip css="absolute bottom-7">This project is {project.public ? "public" : "private"}</CustomTooltip>
+				<div className="w-6 h-6 absolute bottom-2.5 left-2.5 flex items-center justify-center group">
+					{projectPublic ? <Unlock size={20} /> : <Lock size={20} />}
+					<CustomTooltip css="absolute bottom-7">This project is {projectPublic ? "public" : "private"}</CustomTooltip>
 				</div>
 			</div>
 		</div>
 	);
 };
+
+async function ProjectPublic(link: string): Promise<Boolean> {
+	return await fetch(link, { cache: "no-store" }).then((res) => (res.status == 200 ? true : false));
+}
 
 export default ProjectCard;
